22ec03a115b527f0400afc9f5354eac372a2edff
KofiAnnan97 <kofi4annan@gmail.com>
2025-03-07 18:37:03 -0500

    Set up serial commands.

--- test_servo/test_servo.ino
+++ test_servo/test_servo.ino
@@ -4,55 +4,89 @@
  
 #define SERVO_PIN 14
 
-Servo headservo; 
+#define DEBUG 0
 
-int pos = 0;   
-int iter_num = 0;
-int last_pos = -1;
+Servo headservo;
+
+/*
+  TODOs:
+    - figure out how to deal with the digital servo reading the wrong position
+*/
+
+
+/* Variables */
+const int centerPos = 90;  
+
+String command;
+
+void helpMenu(){
+  Serial.println("The following commands are supported:");
+  Serial.println("\t- \"center\"     : sets servo to center angle");
+  Serial.println("\t- \"sweep\"      : test velocity control for all wheels");
+  Serial.println("\t- \"read\"       : get the current servo angle");
+  Serial.println("\t- angle_val      : set servo to a specific angle (integer)");
+  Serial.println("\t- \"help\"       : bring up this menu");
+}
 
 void setup() { 
   headservo.attach(SERVO_PIN);  
-  Serial.begin(9600);
+  Serial.begin(115200);
+  helpMenu();
 } 
  
-int get_head_angle() { return headservo.read(); }
+int getHeadAngle() { return headservo.read(); }
 
-void reset_center(){
-  int curr_pos = get_head_angle();
-  int target_pos = 90;
-  //int target_pos = 0;
-  if(target_pos > curr_pos) {
-    for(int pos = curr_pos; pos <= target_pos; pos += 1) {                                 
+void resetCenter(){
+  int currPos = getHeadAngle();
+    if(centerPos > currPos) {
+    for(int pos = currPos; pos <= centerPos; pos += 1) {                                 
       headservo.write(pos);
       delay(15);                      
     } 
   } 
-  else if(target_pos < curr_pos) {
-    for(int pos = curr_pos; pos >= target_pos; pos -= 1) {                                
+  else if(centerPos < currPos) {
+    for(int pos = currPos; pos >= centerPos; pos -= 1) {                                
       headservo.write(pos);
       delay(15);                     
     }
   }
 }
 
-void set_head(int target_pos) {
-  if(target_pos > 180) target_pos = 180;
-  else if(target_pos < 0) target_pos = 0;
+void sweep(){
+  int pos;
+  for(pos = 0; pos <= 180; pos += 20) {                                 
+    setHead(pos);
+    Serial.print("Target Position: ");
+    Serial.println(getHeadAngle());
+    delay(500); 
+  } 
+  for(pos = 180; pos >= 0; pos -= 20) {                                 
+    setHead(pos);
+    Serial.print("Target Position: ");
+    Serial.println(getHeadAngle());
+    delay(500); 
+  } 
+}
 
-  int curr_pos = get_head_angle();
-  Serial.print("Current Pos: ");
-  Serial.print(curr_pos);
-  Serial.print(" | Target Pos: ");
-  Serial.println(target_pos);
+void setHead(int targetPos) {
+  if(targetPos > 180) targetPos = 180;
+  else if(targetPos < 0) targetPos = 0;
 
-  if(target_pos >= curr_pos) {
-    for(int pos = curr_pos; pos <= target_pos; pos += 1) {                                  
+  int currPos = getHeadAngle();
+  if(DEBUG == 1){
+    Serial.print("Current Pos: ");
+    Serial.print(currPos);
+    Serial.print(" | Target Pos: ");
+    Serial.println(targetPos);
+  }
+  if(targetPos > currPos) {
+    for(int pos = currPos; pos <= targetPos; pos += 1) {                                  
       headservo.write(pos);
       delay(15);                      
     } 
   } 
-  else if(target_pos < curr_pos) {
-    for(int pos = curr_pos; pos >= target_pos; pos -= 1) {                                
+  else if(targetPos < currPos) {
+    for(int pos = currPos; pos >= targetPos; pos -= 1) {                                
       headservo.write(pos);
       delay(15);                     
     }
@@ -64,18 +98,31 @@ void set_head(int target_pos) {
 }
 
 void loop() { 
-  for(pos = 0; pos <= 180; pos += 20) {                                 
-    set_head(pos);
-    Serial.print("Position: ");
-    Serial.println(get_head_angle());
-    delay(2000); 
+  // Send commands to servo
+  if(Serial.available()){
+    command = Serial.readStringUntil('\n');
+    command.trim();
+    Serial.print("\nCommand: ");
+    Serial.println(command);
+    // Bring up help menu
+    if(command.equals("help")) helpMenu();
+    // Retrieve current servo angle
+    else if(command.equals("read")){
+      int currPos = getHeadAngle();
+      Serial.print("Current position: ");
+      Serial.println(currPos);
+    } 
+    // Set servo angle to center position
+    else if(command.equals("center")) resetCenter();
+    // Go from 0 to 180 back to 0 [degrees]
+    else if(command.equals("sweep")) sweep();
+    // Set a servo to a desired angle
+    else{
+      int position = command.toInt();
+      setHead(position);
+      Serial.print("Desired position: ");
+      Serial.println(position);
+    }
   } 
-  for(pos = 180; pos >= 0; pos -= 20) {                                 
-    set_head(pos);
-    Serial.print("Position: ");
-    Serial.println(get_head_angle());
-    delay(2000); 
-  } 
-  reset_center();
-  delay(5000);
+  delay(1000);
 } 
